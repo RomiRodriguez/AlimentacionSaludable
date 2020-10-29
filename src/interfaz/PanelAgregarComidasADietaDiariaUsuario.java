f84469a88249f56f94671c19a518bc6e91a5de20
Misos14 <diegozynga@gmail.com>
2020-10-28 22:49:35 -0300

    Solicitud de cambio nro. 6

--- src/interfaz/PanelAgregarComidasADietaDiariaUsuario.java
+++ src/interfaz/PanelAgregarComidasADietaDiariaUsuario.java
@@ -26,15 +26,30 @@ public class PanelAgregarComidasADietaDiariaUsuario extends javax.swing.JPanel {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        btnAgregarComida = new javax.swing.JButton();
         jScrollPane1 = new javax.swing.JScrollPane();
         listaComidasDiarias = new javax.swing.JList();
         etiquetaTitulo = new javax.swing.JLabel();
-        btnAgregarComida = new javax.swing.JButton();
         btnVolver = new javax.swing.JButton();
+        textoAviso = new javax.swing.JLabel();
 
         setBackground(new java.awt.Color(255, 255, 255));
         setPreferredSize(new java.awt.Dimension(1147, 784));
         setLayout(null);
+
+        btnAgregarComida.setBackground(new java.awt.Color(255, 0, 102));
+        btnAgregarComida.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
+        btnAgregarComida.setForeground(new java.awt.Color(255, 255, 255));
+        btnAgregarComida.setText("Agregar Comida");
+        btnAgregarComida.setContentAreaFilled(false);
+        btnAgregarComida.setOpaque(true);
+        btnAgregarComida.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnAgregarComidaActionPerformed(evt);
+            }
+        });
+        add(btnAgregarComida);
+        btnAgregarComida.setBounds(420, 540, 210, 35);
 
         listaComidasDiarias.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
         jScrollPane1.setViewportView(listaComidasDiarias);
@@ -49,20 +64,6 @@ public class PanelAgregarComidasADietaDiariaUsuario extends javax.swing.JPanel {
         add(etiquetaTitulo);
         etiquetaTitulo.setBounds(390, 10, 270, 50);
 
-        btnAgregarComida.setBackground(new java.awt.Color(255, 0, 102));
-        btnAgregarComida.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
-        btnAgregarComida.setForeground(new java.awt.Color(255, 255, 255));
-        btnAgregarComida.setText("Agregar Comida");
-        btnAgregarComida.setContentAreaFilled(false);
-        btnAgregarComida.setOpaque(true);
-        btnAgregarComida.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btnAgregarComidaActionPerformed(evt);
-            }
-        });
-        add(btnAgregarComida);
-        btnAgregarComida.setBounds(660, 540, 210, 37);
-
         btnVolver.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/volver.png"))); // NOI18N
         btnVolver.setBorderPainted(false);
         btnVolver.setContentAreaFilled(false);
@@ -73,6 +74,15 @@ public class PanelAgregarComidasADietaDiariaUsuario extends javax.swing.JPanel {
         });
         add(btnVolver);
         btnVolver.setBounds(0, 10, 110, 70);
+
+        textoAviso.setFont(new java.awt.Font("Segoe UI", 0, 24)); // NOI18N
+        textoAviso.setForeground(new java.awt.Color(255, 0, 153));
+        textoAviso.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        textoAviso.setText("texto");
+        textoAviso.setToolTipText("");
+        textoAviso.setVisible(false);
+        add(textoAviso);
+        textoAviso.setBounds(280, 600, 520, 40);
     }// </editor-fold>//GEN-END:initComponents
 
     private void btnAgregarComidaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnAgregarComidaActionPerformed
@@ -80,6 +90,17 @@ public class PanelAgregarComidasADietaDiariaUsuario extends javax.swing.JPanel {
         interfaz.getUsuarioActual().getHistorialDelDia().getComidasIngeridas().add(comidaSeleccionada);
         LocalDate diaActual = LocalDate.now();
         interfaz.getUsuarioActual().setFechaUltimaAdicion(diaActual.toString());
+        if (listaComidasDiarias.isSelectionEmpty()){
+        //mostrar que no se aggrego nada
+        textoAviso.setText("No se agregó ninguna comida");
+        
+        }else{
+        //mostrar que se agrego algo
+        textoAviso.setText("La comida se agregó correctamente");
+        }
+        textoAviso.setVisible(true);
+        
+        
     }//GEN-LAST:event_btnAgregarComidaActionPerformed
 
     private void btnVolverActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnVolverActionPerformed
@@ -97,5 +118,6 @@ public class PanelAgregarComidasADietaDiariaUsuario extends javax.swing.JPanel {
     private javax.swing.JLabel etiquetaTitulo;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JList listaComidasDiarias;
+    private javax.swing.JLabel textoAviso;
     // End of variables declaration//GEN-END:variables
 }
